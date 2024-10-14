b12410de7326bf236ca890d03b77c87bc6cf1d60
omarenriquecsn <omarenriquecs@gmail.com>
2024-10-13 21:35:29 -0400

    actualizacion

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 var pantalla = [];
 var pantalla2 = 0;
 var multi = 1;
-var signos = '';
-var numeros = [0,1,2,3,4,5,6,7,8,9,];
+console.log(pantalla);
+
+var numeros = ['0','1','2','3','4','5','6','7','8','9',];
 var chart = ['+','-','*','/'];
-document.querySelector('boton');
-this.addEventListener('click', function(event){
-   if(numeros.includes(Number(event.target.textContent))){pantalla.push(event.target.textContent)}
-   if(chart.includes(event.target.textContent)){signos = (event.target.textContent)}
-    this.document.getElementById('pantalla').textContent = Number(pantalla.join(''))
+
+
+document.addEventListener('click',(event)=>{
+    event.preventDefault();
     
+    let signos = event.target.textContent === "=";
+    let laPantalla1 = document.getElementById('pantalla');
+    let laPantalla2 = document.getElementById('pantalla2');
+    let numero = event.target.textContent
+    
+    if(numeros.includes(numero)){
+        pantalla.push(numero);
+        laPantalla1.textContent = Number(pantalla.join(""))  
+    }
+    else if(numero === "+"){
+            pantalla2 += Number(pantalla.join(""));
+            pantalla = [];
+            laPantalla2.textContent = pantalla2;
+            laPantalla1.textContent = pantalla;        
+            
+          
+        }
+        else if(numero === "-"){
+            
+            pantalla2 -= Number(pantalla.join(""));
+            pantalla = [];
+            laPantalla2.textContent = pantalla2;
+            laPantalla1.textContent = pantalla;        
+            }
+            else if(numero === "*"){
+                if(pantalla2 === 0){
+                    pantalla2 = Number(pantalla.join(""));}
+                    else if(pantalla.length == 0){}
+                    else{
+                        pantalla2 = Number(pantalla.join(""))*pantalla2
+                    }
+                pantalla = [];
+                laPantalla2.textContent = pantalla2;
+                laPantalla1.textContent = pantalla;        
+                }
+                else if(numero === "/"){
+                    if(pantalla2 === 0){
+                        pantalla2 = Number(pantalla.join(""));}
+                        else if(pantalla.length== 0){}
+                        else{
+                            pantalla2 = pantalla2/Number(pantalla.join(""))
+                        }
+                    pantalla = [];
+                    laPantalla2.textContent = pantalla2;
+                    laPantalla1.textContent = pantalla;        
+                    }
+                    else if(numero == 'AC'){
+                        pantalla =[];
+                        laPantalla1.textContent = pantalla
+
+                    }
+                    else if(numero == 'ACC'){
+                        pantalla =[];
+                        laPantalla1.textContent = pantalla
+                        pantalla2 = 0;
+                        laPantalla2.textContent = pantalla2
+
+                    }
+                    
+
 })
 
-    document.querySelector('botons');
-    this.addEventListener("click", function(event){
-        if(event.target.textContent == "+"){
-            pantalla2 = Number(pantalla.join(''))+pantalla2;
-        this.document.getElementById('pantalla2').textContent = pantalla2;
-        this.document.getElementById('pantalla').textContent = 0;
-        pantalla = [];}
-        else if(signos === '+'&& event.target.textContent == '='){
-            this.document.getElementById('pantalla2').textContent = Number(pantalla.join(''))+pantalla2;
-            this.document.getElementById('pantalla').textContent = 0;
-            pantalla = []
-        }
-
-    })
-    document.querySelector('botonme');
-    this.addEventListener("click", function(event){
-        if(event.target.textContent == "-"){
-            pantalla2 = Math.abs(Number(pantalla.filter((x)=>x!== '-').join(''))-pantalla2)
-        this.document.getElementById('pantalla2').textContent = pantalla2;
-        this.document.getElementById('pantalla').textContent = 0;
-        pantalla = [];}
-        else if(signos === '-'&& event.target.textContent == '='){
-            this.document.getElementById('pantalla2').textContent = Math.abs(Number(pantalla.filter((x)=>x!== '-').join(''))-pantalla2);
-            this.document.getElementById('pantalla').textContent = 0;
-            pantalla = []
-        }
-
-    })
-  
-    document.querySelector('botonmu');
-    this.addEventListener("click", function(event){
-        if(event.target.textContent == "*"){
-            pantalla2 = Number(pantalla.join(''))*pantalla2;
-            multi = pantalla2
-        this.document.getElementById('pantalla2').textContent = pantalla2;
-        this.document.getElementById('pantalla').textContent = 0;
-        pantalla = [];}
-        else if(signos === '*'&& event.target.textContent == '='){
-            this.document.getElementById('pantalla2').textContent = Number(pantalla.filter((x)=>x!== '*').join(''))*multi;
-            this.document.getElementById('pantalla').textContent = 0;
-            pantalla = [];
-            multi = 1;
-        }
-    })
-    document.querySelector('botons');
-    this.addEventListener("click", function(event){
-        if(event.target.textContent == "/"){
-            pantalla2 = Number(pantalla.join(''))/pantalla2;
-            multi = pantalla2
-        this.document.getElementById('pantalla2').textContent = pantalla2;
-        this.document.getElementById('pantalla').textContent = 0;
-        pantalla = [];}
-        else if(signos === '/'&& event.target.textContent == '='){
-            this.document.getElementById('pantalla2').textContent = pantalla2/Number(pantalla.join(''));
-            this.document.getElementById('pantalla').textContent = 0;
-            pantalla = []
-            multi = 1;
-        }
-
-    })
 
 
